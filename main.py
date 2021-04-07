27f626dc7a0f7d9d8e40a78c53f98b8f6a6c98b8
Ismaeel <ismaeelakram01@gmail.com>
2021-04-07 00:44:15 -0400

    Fix a lot of bugs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webbrowser
 import time
 import os
 from selenium import webdriver
+import sys
 
 chrome_path = "open -a /Applications/Google\ Chrome.app %s"
 browser = webbrowser.get(chrome_path)
@@ -13,24 +14,27 @@ for link in open("links.txt").readlines():
     open_time = link.split("|")[1]
     links.append((url, open_time, False))
 
+print("Found links: " + str(links))
+time.sleep(3)
+
 while True:
     timestamp = time.strftime("%H:%M")
+    print(timestamp)
+    sys.stdout.flush()
     for link in links:
-        if timestamp == link[1] or link[1] == "test":
-            if link[2] == False:
-                if "meet.google.com" in link[0]:
-                    browser = webdriver.Chrome()
-                    browser.get(link)
+        if str(timestamp) == link[1] and link[2] == False: 
+            if "meet.google.com" in link[0]:
+                browser = webdriver.Chrome()
+                browser.get(link)
 
-                    join_btn = browser.find_element_by_xpath(
-                        '//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]'
-                    )
-                    join_btn.click()
-                else:
-                    browser.open_new_tab(link[0])
-                    links[links.index(link)] = (link[0], link[1], True)
-                    os.system("say New link has been opened.")
+                join_btn = browser.find_element_by_xpath(
+                    '//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]'
+                )
+                join_btn.click()
+                links[links.index(link)] = (link[0], link[1], True)
+                os.system("say New link has been opened.")
             else:
-                print("Link has already been opened.")
-
-                    
+                browser.open_new_tab(link[0])
+                links[links.index(link)] = (link[0], link[1], True)
+                os.system("say New link has been opened.")
+                
